ea7f5a63d6f0bca773716c17c4bcb032220424d1
jhonviafara <jhon1144105@gmail.com>
2024-05-25 23:58:33 -0300

    Add dotenv configuration and update .gitignore

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,23 @@
-//primero voy a importar la funciotion de createConnection desde el paquete mysql que se encuentra en los modulos de node
 import { createConnection } from "mysql"
-//creamos una constante coneccion la cual llama a la funcion createConneccion y le que a su vez y tiene como parametro un objeto con los datos necesarios para realizar dicha connexion : host,usuario,password y database.  
+//importar la funciotion de createConnection del paquete mysql 
+import dotenv from "dotenv";
+// Cargar variables de entorno desde un archivo .env
+dotenv.config();
+
+//constante coneccion llama a la funcion createConneccion y recive parametro un objeto con los datos necesarios para realizar dicha connexion : host,usuario,password y database.  
 const connection = createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'aun no ',
-    database:'aun no'
+    host:process.env.DB_HOST || 'localhost',
+    user:process.env.DB_USER || '',
+    password:process.env.DB_PASSWORD || '',
+    database:process.env.DB_NAME || ''
 })
-connection.connect((err) =>{
-    if (err) {
-    console.error('error conectando a la base de datos', err)
-} 
-    console.log("conexion a la base de datos establecida")
-})
+    connection.connect((err) =>{
+        if (err) {
+            console.error('error conectando a la base de datos', err)
+            return
+        } 
+        console.log("conexion a la base de datos establecida")
+    })
+    
+//exportar la conexion para usarla en otros archivos
+export default connection;
